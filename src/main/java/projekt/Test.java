94b3ba81baa6becb500c17f58619c4ce5df62d3f
Pawcio <Pawcio@Pawcio-Komputer>
2016-05-30 11:40:07 +0200

    zadanie 3

---
+++ src/main/java/projekt/Test.java
@@ -0,0 +1,15 @@
+package projekt;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Licz licz=new Licz();
+		System.out.println(licz);
+		
+		Licz l = new Licz();
+		System.out.println(l.liczymy(9, 4));
+
+	}
+
+}
